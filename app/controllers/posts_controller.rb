87cecd24cc6ce8f6e14811a33a88452d361408ac
Ivan <collado.ip@gmail.com>
2016-11-18 04:27:47 +0000

    add post filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.map.with_index{ |post, index| post.title = "SPAM" unless index % 5 != 0 }
   end
 
   def show
